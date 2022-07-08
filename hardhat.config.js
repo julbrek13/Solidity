1010de114457c571d02157dba62bec9a14ee952c
YPC <julianmacarron@hotmail.com>
2022-07-08 08:59:06 -0300

    intento de deploy in rinkeby}

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +20,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      //1337,
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.DEPLOY_KEY_RINKEBY}`,
+      accounts: [process.env.DEPLOY_ACC_RINKEBY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,18 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contract with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+  //lee el archivo de contrato
   const Simplestorage = await hre.ethers.getContractFactory("Simplestorage");
-  const simplestorage = await Simplestorage.deploy("...");
-  console.log("el numero es:", simplestorage.number);
+  //activa el despliegue
+  const simplestorage = await Simplestorage.deploy({});
+  //espera a que finalice el despliegue
   await simplestorage.deployed();
+  console.log("el numero de contrato es:", simplestorage.number);
   console.log("Simplestorage deployed to:", simplestorage.address);
 }
 main()
